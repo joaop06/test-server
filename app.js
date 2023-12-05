cc8573ac02359838dd94efc2d4b1eb0c223a3df6
joaop06 <joaoofficialpedro@gmail.com>
2023-12-05 18:52:06 -0300

    test

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000')
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello Word!')
+})
+
+app.get('/test', (req, res) => {
+    res.send({
+        name: 'João Pedro',
+        age: 20
+    })
+})
